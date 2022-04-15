2b9fece52c0494f30a4234b512b0def3cc5157ef
darqus <voladores@inbox.ru>
2022-04-15 22:28:50 +0300

    fix getHighlightedStringFromPhrase

--- src/helpers/getHighlightedStringFromPhrase.ts
+++ src/helpers/getHighlightedStringFromPhrase.ts
@@ -7,9 +7,7 @@ export const getHighlightedStringFromPhrase = (phrase: string, string: string) =
   if (!phrase && !string) return
 
   const firstLettersOfWord = getFirstLettersOfWord(phrase, string)
-
   const slpittedPhrase = phrase.split(firstLettersOfWord)
-
   const result = [slpittedPhrase[0], firstLettersOfWord, slpittedPhrase[1]]
 
   return result
