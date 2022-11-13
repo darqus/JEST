41fe2adf995a014fe8ef9fb37b9c4c387643c288
darqus <voladores@inbox.ru>
2022-11-13 08:17:13 +0300

    ref testFormRE

--- src/helpers/testFormRE.ts
+++ src/helpers/testFormRE.ts
@@ -1,6 +1,13 @@
 const RE_EMAIL = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-const RE_RASSWORD =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8}$/
+
+const PWD_MAP = {
+  MIN: 6,
+  MAX: 20,
+  SPC: '!@#$%^&*',
+}
+const RE_RASSWORD = new RegExp(
+  `^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[${PWD_MAP.SPC}])[A-Za-z\\d${PWD_MAP.SPC}]{${PWD_MAP.MIN},${PWD_MAP.MAX}}$`,
+)
 
 export const testFormEmail = (prop: string) => RE_EMAIL.test(prop)
 export const testFormRassword = (prop: string) => RE_RASSWORD.test(prop)
--- tests/module/testFormRE.spec.ts
+++ tests/module/testFormRE.spec.ts
@@ -4,10 +4,20 @@ import { testFormEmail, testFormRassword } from '../../src/helpers/testFormRE'
 describe('testFormRE()', () => {
   it('testFormEmail()', () => {
     expect(testFormEmail('example@domain.zone')).toBe(true)
+    expect(testFormEmail('example@domain.z')).toBe(false)
+    expect(testFormEmail('john.doe.twitter.org')).toBe(false)
     expect(testFormEmail('john.doe@twitter.org')).toBe(true)
   })
   it('testFormRassword()', () => {
-    expect(testFormRassword('Test022#')).toBe(true)
+    expect(testFormRassword('Te0@')).toBe(false)
+    expect(testFormRassword('Testtttttttttttttttttttttttttttttt0@')).toBe(false)
     expect(testFormRassword('Test022!')).toBe(true)
+    expect(testFormRassword('Test022@')).toBe(true)
+    expect(testFormRassword('Test022#')).toBe(true)
+    expect(testFormRassword('Test022$')).toBe(true)
+    expect(testFormRassword('Test022%')).toBe(true)
+    expect(testFormRassword('Test022^')).toBe(true)
+    expect(testFormRassword('Test022&')).toBe(true)
+    expect(testFormRassword('Test022*')).toBe(true)
   })
 })
