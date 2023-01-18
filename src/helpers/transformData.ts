3c35468dd8b27eb68fcb978f6eaeebc60b2f8960
darqus <voladores@inbox.ru>
2023-01-18 03:50:00 +0300

    add transformData

---
+++ src/helpers/transformData.ts
@@ -0,0 +1,33 @@
+export const isMatchesStringFromPhrase = (phrase: string, string: string) =>
+  phrase
+    .toLowerCase()
+    .split(' ')
+    .some((_it) => _it.startsWith(string.toLowerCase()))
+
+const getFirstLettersOfWord = (phrase: string, string: string) =>
+  !phrase && !string
+    ? ''
+    : phrase
+        .split(' ')
+        .find((_it: string) =>
+          _it.toLowerCase().startsWith(string.toLowerCase()),
+        )
+        .substring(0, string.length)
+
+export const getHighlightedStringFromPhrase = (
+  phrase: string,
+  string: string,
+) => {
+  let result = []
+  if (!phrase && !string) {
+    result = []
+  }
+
+  const firstLettersOfWord: string = getFirstLettersOfWord(phrase, string)
+  const slpittedPhrase: Array<string> = phrase.split(firstLettersOfWord)
+  result = [slpittedPhrase[0], firstLettersOfWord, slpittedPhrase[1]]
+
+  return result
+}
+
+export const getIDfromApiUrl = (url: string) => +url.split('/').reverse()[1]
---
+++ tests/module/transformData.spec.ts
@@ -0,0 +1,42 @@
+import { describe, it, expect } from '@jest/globals'
+import { getHighlightedStringFromPhrase } from '../../src/helpers/transformData'
+
+describe('isPairBrackets()', () => {
+  it('isPairBrackets()', () => {
+    expect(getHighlightedStringFromPhrase('Luke Skywalker', 'lu')).toEqual([
+      '',
+      'Lu',
+      'ke Skywalker',
+    ])
+    expect(getHighlightedStringFromPhrase('Luke Skywalker', 'sk')).toEqual([
+      'Luke ',
+      'Sk',
+      'ywalker',
+    ])
+    expect(getHighlightedStringFromPhrase('Leia Organa', 'le')).toEqual([
+      '',
+      'Le',
+      'ia Organa',
+    ])
+    expect(getHighlightedStringFromPhrase('Leia Organa', 'or')).toEqual([
+      'Leia ',
+      'Or',
+      'gana',
+    ])
+    expect(getHighlightedStringFromPhrase('C-3PO', 'c')).toEqual([
+      '',
+      'C',
+      '-3PO',
+    ])
+    expect(getHighlightedStringFromPhrase('R2-D2', 'r')).toEqual([
+      '',
+      'R',
+      '2-D2',
+    ])
+    expect(getHighlightedStringFromPhrase('R5-D4', 'r')).toEqual([
+      '',
+      'R',
+      '5-D4',
+    ])
+  })
+})
