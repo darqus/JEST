e89e9f9b739bdf2978459bf4cc997667aef72a24
darqus <voladores@inbox.ru>
2023-03-29 13:06:01 +0300

    add isNumber

---
+++ tests/module/isNumber.spec.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect } from '@jest/globals'
+import { isNumber } from '../../src/helpers/isNumber'
+
+describe('isNumber()', () => {
+  it('isNumber()', () => {
+    expect(isNumber(0)).toBe(true)
+    expect(isNumber(0.0)).toBe(true)
+    expect(isNumber(-0)).toBe(true)
+    expect(isNumber(1)).toBe(true)
+    expect(isNumber(1.0)).toBe(true)
+    expect(isNumber(-1.0)).toBe(true)
+    expect(isNumber(0.1 * 0.2)).toBe(true)
+    expect(isNumber(-0.1 * 0.4)).toBe(true)
+    expect(isNumber(NaN)).toBe(false)
+    // expect(isNumber('0')).toBe(true)
+    // expect(isNumber(undefined)).toBe(false)
+    // expect(isNumber('undefined')).toBe(false)
+    // expect(isNumber(null)).toBe(false)
+    // expect(isNumber('null')).toBe(false)
+    // expect(isNumber(true)).toBe(false)
+    // expect(isNumber(false)).toBe(false)
+  })
+})
