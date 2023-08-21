70b71ace392528f1b2fdaf14291adaf1ff4e5dfb
darqus <voladores@inbox.ru>
2023-08-21 04:06:39 +0300

    fix isMagicMathExpression

--- src/helpers/isMagicMathExpression.ts
+++ src/helpers/isMagicMathExpression.ts
@@ -1,5 +1,5 @@
 // const MAGIC_STRINGS = ['000000000000000', '00000000000000' ,'999999999999999']
 const RE_MAGIC_MATH_EXPRESSION = /[0,9]{14,15}/
 
-export const isMagicMathExpression = (prop: number) =>
+export const isMagicMathExpression = (prop: number): boolean =>
   RE_MAGIC_MATH_EXPRESSION.test(prop.toString())
