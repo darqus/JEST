97a30adddc593e7564fdfdb3ddcbb7de5b817ed9
darqus <voladores@inbox.ru>
2023-10-09 19:06:46 +0300

    fix optional-parameters-in-callbacks

--- src/code-basics/optional-parameters-in-callbacks.ts
+++ src/code-basics/optional-parameters-in-callbacks.ts
@@ -4,15 +4,15 @@
 const map = (set: number[], callback: (n: number, idx?: number) => number) => {
   const result: number[] = []
 
-  for (const [ i, ] of Object.entries(set)) {
+  /* for (const [ i, ] of Object.entries(set)) {
     result.push(callback(set[i], +i))
-  }
+  } */
 
   // set.forEach((n, index) => result.push(callback(n, index)))
 
-  /* for (let i = 0; i < set.length; i++) {
+  for (let i = 0; i < set.length; i++) {
     result.push(callback(set[i], i))
-  } */
+  }
 
   return result
 }
