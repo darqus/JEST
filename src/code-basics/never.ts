45bc33504af26d87a1975805c62d7fa2bb3c0c72
darqus <voladores@inbox.ru>
2023-10-09 17:20:05 +0300

    fix never

--- src/code-basics/never.ts
+++ src/code-basics/never.ts
@@ -1,8 +1,8 @@
 // https://code-basics.com/ru/languages/typescript/lessons/never
 
 // BEGIN (write your solution here)
-const fail = (): never => {
-  throw new Error('error')
+const fail = (message: string): never => {
+  throw new Error(message)
 }
 // END
 
--- tests/code-basics/never.spec.ts
+++ tests/code-basics/never.spec.ts
@@ -3,7 +3,7 @@ import { describe, it, expect, } from '@jest/globals'
 import fail from '../../src/code-basics/never'
 
 describe('fail', () => {
-  it('should throw an error', () => {
-    expect(() => fail()).toThrowError('error')
+  it('should throw an error with the provided message', () => {
+    expect(() => fail('Test error message')).toThrowError('Test error message')
   })
 })
