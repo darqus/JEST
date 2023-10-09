8489e62e8c1caa9f9d764b1caa048344d5099ce9
darqus <voladores@inbox.ru>
2023-10-09 19:02:24 +0300

    add optional-parameters-in-callbacks

---
+++ src/code-basics/optional-parameters-in-callbacks.ts
@@ -0,0 +1,21 @@
+// https://code-basics.com/ru/languages/typescript/lessons/optional-parameters-in-callbacks
+
+// BEGIN (write your solution here)
+const map = (set: number[], callback: (n: number, idx?: number) => number) => {
+  const result: number[] = []
+
+  for (const [ i, ] of Object.entries(set)) {
+    result.push(callback(set[i], +i))
+  }
+
+  // set.forEach((n, index) => result.push(callback(n, index)))
+
+  /* for (let i = 0; i < set.length; i++) {
+    result.push(callback(set[i], i))
+  } */
+
+  return result
+}
+// END
+
+export default map
---
+++ tests/code-basics/optional-parameters-in-callbacks.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect, } from '@jest/globals'
+
+import map from '../../src/code-basics/optional-parameters-in-callbacks'
+
+test('map', () => {
+  const result = map([], (n: number) => n + 3)
+
+  expect(result).toEqual([])
+
+  const result2 = map([ 3, 9, ], (n: number) => n - 3)
+
+  expect(result2).toEqual([ 0, 6, ])
+
+  const result3 = map([ 8, 9, ], (n: number) => n + 8)
+
+  expect(result3).toEqual([ 16, 17, ])
+
+  const result4 = map([ 10, 10, 10, ], (n: number, index) => n + index)
+
+  expect(result4).toEqual([ 10, 11, 12, ])
+})
