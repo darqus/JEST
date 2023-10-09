3818728c47890e9f0c05b9d67ed91a3009139312
darqus <voladores@inbox.ru>
2023-10-09 16:28:34 +0300

    add destructuring

---
+++ tests/code-basics/destructuring.spec.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect, } from '@jest/globals'
+
+import { lessonsCount, } from '../../src/code-basics/destructuring'
+
+describe('lessonsCount', () => {
+  it('should return 0 when lessons array is empty', () => {
+    expect(lessonsCount({ lessons: [], })).toBe(0)
+  })
+
+  it('should return the number of lessons when lessons array is not empty', () => {
+    expect(lessonsCount({ lessons: [ 'lesson1', 'lesson2', 'lesson3', ], })).toBe(3)
+  })
+})
