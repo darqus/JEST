f7cfcff106582d35685b15cd54a2ffe96cd593ab
darqus <voladores@inbox.ru>
2023-10-10 11:49:41 +0300

    fix type-aliases

--- src/code-basics/type-aliases.ts
+++ src/code-basics/type-aliases.ts
@@ -9,11 +9,7 @@ const getOlderUser = (user1: User, user2: User) => {
   const age1 = user1.age
   const age2 = user2.age
 
-  if (age1 === age2) {
-    return null
-  }
-
-  return age1 > age2 ? user1 : user2
+  return age1 === age2 ? null : age1 > age2 ? user1 : user2
 }
 
 // END
