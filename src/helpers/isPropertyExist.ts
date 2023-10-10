c88c4146a2b5621b5e74a181b5f0ad1cd54ad978
darqus <voladores@inbox.ru>
2023-10-11 00:45:57 +0300

    add JSDocs to isPropertyExist

--- src/helpers/isPropertyExist.ts
+++ src/helpers/isPropertyExist.ts
@@ -3,4 +3,10 @@ export type Person = {
   salary: number
 }
 
+/**
+ * Checks if a property exists in an object.
+ * @param object - The object to check.
+ * @param property - The property to check for.
+ * @returns True if the property exists in the object, false otherwise.
+ */
 export const isPropertyExist = (object: Person, property: string): boolean => property in object
