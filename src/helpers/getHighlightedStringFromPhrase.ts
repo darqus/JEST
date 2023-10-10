d6340a64d461f66cf394f7ff7c99139068e42ff9
darqus <voladores@inbox.ru>
2023-10-11 00:39:17 +0300

    ref  getHighlightedStringFromPhrase

--- src/helpers/getHighlightedStringFromPhrase.ts
+++ src/helpers/getHighlightedStringFromPhrase.ts
@@ -1,18 +1,33 @@
-const getFirstLettersOfWord = (phrase: string, string: string) =>
-  phrase
-    ?.split(' ')
-    ?.find((it) => it.toLowerCase().startsWith(string.toLowerCase()))
-    ?.substring(0, string?.length)
+/**
+ * Returns the first letters of a word in a given phrase that match a search string.
+ *
+ * @param {string} phrase - The phrase to search within.
+ * @param {string} searchString - The string to search for in the phrase.
+ * @returns {string | undefined} - The first letters of the matching word, or undefined if no match is found.
+ */
+function getFirstLetters (phrase: string, searchString: string): string | undefined {
+  const words: string[] = phrase.split(' ')
+  const matchingWord: string | undefined = words.find((word: string) =>
+    word.toLowerCase().startsWith(searchString.toLowerCase())
+  )
 
+  return matchingWord ? matchingWord.substring(0, searchString.length) : undefined
+}
+
+/**
+ * Generates a highlighted string from a given phrase and string.
+ *
+ * @param {string} phrase - The phrase to search for in the string.
+ * @param {string} string - The string to search within.
+ * @returns {string[]} An array containing the highlighted string.
+ */
 export const getHighlightedStringFromPhrase = (
   phrase: string,
   string: string
-) => {
-  if (!phrase && !string) return
-
-  const firstLettersOfWord = getFirstLettersOfWord(phrase, string)
-  const slpittedPhrase = phrase.split(firstLettersOfWord)
-  const result = [ slpittedPhrase[0], firstLettersOfWord, slpittedPhrase[1], ]
+): string[] => {
+  const firstLetters: string = getFirstLetters(phrase, string)
+  const splittedPhrase: string[] = phrase.split(firstLetters)
+  const result: string[] = [ splittedPhrase[0], firstLetters, splittedPhrase[1], ]
 
   return result
 }
