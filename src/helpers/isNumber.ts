dcbe0736497a5f12df1eab5a0197e05cde80265c
darqus <voladores@inbox.ru>
2023-10-11 00:04:38 +0300

    add JSDocs to isNumber

--- src/helpers/isNumber.ts
+++ src/helpers/isNumber.ts
@@ -1,1 +1,7 @@
+/**
+ * Checks if a value is a number.
+ *
+ * @param value The value to check.
+ * @returns True if the value is a number, false otherwise.
+ */
 export const isNumber = (value: unknown): value is number => typeof value === 'number'
