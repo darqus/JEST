a2b99220922159d1622705440481f64afbe98753
darqus <voladores@inbox.ru>
2023-10-15 21:24:39 +0300

    fix anonymous-functions

--- src/code-basics/anonymous-functions.ts
+++ src/code-basics/anonymous-functions.ts
@@ -1,5 +1,11 @@
 // https://code-basics.com/ru/languages/typescript/lessons/anonymous-functions
 
+// Анонимные функции 5/53
+
+/* Задание
+
+Напишите функцию, которая возвращает массив четных чисел из массива numbers. */
+
 // BEGIN (write your solution here)
 export const filterEvenNumbers = (numberList: number[]): number[] => numberList.filter((number) => number % 2 === 0)
 
