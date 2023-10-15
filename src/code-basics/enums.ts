a3ba48b5473bd760d2e104c378d00ea015248252
darqus <voladores@inbox.ru>
2023-10-15 21:33:31 +0300

    fix enums

--- src/code-basics/enums.ts
+++ src/code-basics/enums.ts
@@ -1,5 +1,20 @@
 // https://code-basics.com/ru/languages/typescript/lessons/enums
 
+// Перечисления (Enums) 8/53
+
+/* Задание
+
+    Реализуйте перечисление ModalStatus с двумя значениями: Opened и Closed
+    Реализуйте функцию buildModal(). Он возвращает объект, который описывает модальное окно. Параметры функции:
+
+    Текст, который должен быть внутри окна после инициализации
+    Статус, с которым надо создать объект окна
+
+Функция возвращает объект с двумя полями: text (здесь хранится переданный текст) и status (здесь хранится переданный статус)
+
+const modal = buildModal('hexlet forever', ModalStatus.Opened);
+// { text: 'hexlet forever', status: ModalStatus.Opened } */
+
 // BEGIN (write your solution here)
 enum ModalStatus {
   Opened,
