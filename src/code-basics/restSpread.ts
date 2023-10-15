f18eb25c13c60eca5b1ce25670c31633761cbd94
darqus <voladores@inbox.ru>
2023-10-15 21:40:45 +0300

    fix rest-spread

--- src/code-basics/restSpread.ts
+++ src/code-basics/restSpread.ts
@@ -1,4 +1,15 @@
-// https://code-basics.com/ru/languages/typescript/lessons/rest-spread#editor
+// https://code-basics.com/ru/languages/typescript/lessons/rest-spread
+
+// Rest и Spread 18/53
+
+/* Задание
+
+Определите функцию max(), которая отличается от примера из урока только тем, что у нее первый параметр обязательный.
+
+Например:
+
+max(1,2,3);
+max(234); */
 
 // BEGIN (write your solution here)
 const max = (num: number, ...numbers: number[]): number => {
