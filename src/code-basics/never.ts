21ff431926e9fea7b55018e3601c11b6b428fcc3
darqus <voladores@inbox.ru>
2023-10-15 21:38:07 +0300

    fix never

--- src/code-basics/never.ts
+++ src/code-basics/never.ts
@@ -1,5 +1,11 @@
 // https://code-basics.com/ru/languages/typescript/lessons/never
 
+// Тип never (возврат из функции) 15/53
+
+/* Задание
+
+Реализуйте функцию fail(), которая выбрасывает любое исключение. Пропишете ее возвращаемый тип явно. */
+
 // BEGIN (write your solution here)
 const fail = (message: string): never => {
   throw new Error(message)
