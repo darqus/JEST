1049e454e1e1ec50276e2b1c078b01005cd29ec6
darqus <voladores@inbox.ru>
2023-10-15 21:34:36 +0300

    fix function-as-parameter

--- src/code-basics/function-as-parameter.ts
+++ src/code-basics/function-as-parameter.ts
@@ -1,5 +1,20 @@
 // https://code-basics.com/ru/languages/typescript/lessons/function-as-parameter
 
+// Функции как параметры 12/53
+
+/* Задание
+
+Реализуйте функцию filter(), которая принимает на вход массив чисел и предикат. Последний будет использоваться для проверки каждого числа на соответствие требованиям:
+
+const numbers = [1, -5, 2, 3, 4, 133];
+filter(numbers, (n) => n > 3); // [4, 133]
+filter(numbers, (n) => n % 2 == 0); // [2, 4]
+
+Параметры функции:
+
+  1. Массив чисел
+  2. Анонимная функция, которая принимает на вход число и возвращает логическое значение */
+
 // BEGIN (write your solution here)
 
 /* const filter = (set: number[], predicate: (n: number) => boolean): number[] => {
