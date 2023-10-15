cb0ffcdc587f4b11674a34362f201eff5bae2cdc
darqus <voladores@inbox.ru>
2023-10-15 21:32:30 +0300

    fix destructuring

--- src/code-basics/destructuring.ts
+++ src/code-basics/destructuring.ts
@@ -1,5 +1,16 @@
 // https://code-basics.com/ru/languages/typescript/lessons/destructuring
 
+// Деструктуризация 17/53
+
+/* Задание
+
+Реализуйте функцию lessonsCount(), которая принимает на вход курс и возвращает количество лекций внутри него:
+
+const course = { lessons: ['intro', 'lala'] };
+lessonsCount(course); // 2
+
+Используйте внутри деструктуризацию, чтобы извлечь уроки прямо в параметрах функции. */
+
 // BEGIN (write your solution here)
 const lessonsCount = ({ lessons, }: { lessons: string[] }) => lessons.length
 // END
