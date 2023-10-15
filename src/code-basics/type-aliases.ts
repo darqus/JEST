011ea185f0193c585f13a278a08c92d365ad8c39
darqus <voladores@inbox.ru>
2023-10-15 21:41:26 +0300

    fix type-aliases

--- src/code-basics/type-aliases.ts
+++ src/code-basics/type-aliases.ts
@@ -1,5 +1,13 @@
 // https://code-basics.com/ru/languages/typescript/lessons/type-aliases
 
+// Псевдонимы типов (Type Aliases) 9/53
+
+/* Задание
+
+Реализуйте функцию getOlderUser(), которая принимает на вход двух пользователей и возвращает того, который старше. Если пользователи являются ровесниками, то возвращается null:
+
+const user1 = { name: 'Petr', age: 8 }; */
+
 export type User = {
   name: string
   age: number
