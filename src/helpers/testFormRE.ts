67ed0207105be5a547874ef808e1c8fb6434e193
darqus <voladores@inbox.ru>
2023-12-23 19:37:39 +0300

    fix naming

--- src/helpers/testFormRE.ts
+++ src/helpers/testFormRE.ts
@@ -1,4 +1,4 @@
-const EMAIL_RE = {
+const EMAIL_RE_PART = {
   USERNAME: /^[^\s@]/,
   AT: '+@',
   DOMAIN: /[^\s@]/,
@@ -6,9 +6,11 @@ const EMAIL_RE = {
   ZONE: /[A-Za-z]{2,4}$/
 }
 
-const RE_EMAIL = new RegExp(`${EMAIL_RE.USERNAME.source}${EMAIL_RE.AT}${EMAIL_RE.DOMAIN.source}${EMAIL_RE.DOT}${EMAIL_RE.ZONE.source}`)
+const RE_EMAIL = new RegExp(`${EMAIL_RE_PART.USERNAME.source}${EMAIL_RE_PART.AT}${EMAIL_RE_PART.DOMAIN.source}${EMAIL_RE_PART.DOT}${EMAIL_RE_PART.ZONE.source}`)
 
-const PWD_MAP = {
+const isEmailValid = (email: string): boolean => RE_EMAIL.test(email)
+
+const PASSWORD_DETAILS = {
   MIN_LENGTH: 6,
   MAX_LENGTH: 20,
   DIGIT_LENGTH: 1,
@@ -16,18 +18,18 @@ const PWD_MAP = {
   SPC_CHAR: '!@#$%^&*',
 }
 
-const PASSWORD_RE = {
+const PASSWORD_RE_PART = {
   HAS_LETTER: /^(?=.*[A-Z][a-z])/,
-  HAS_DIGIT: new RegExp(`(?=.*\\d){${PWD_MAP.DIGIT_LENGTH},}`),
-  HAS_SPECIAL_CHAR: new RegExp(`(?=.*[${PWD_MAP.SPC_CHAR}]){${PWD_MAP.SPC_LENGTH},}`),
-  IS_VALID: new RegExp(`[A-Za-z\\d${PWD_MAP.SPC_CHAR}]{${PWD_MAP.MIN_LENGTH},${PWD_MAP.MAX_LENGTH}}$`)
+  HAS_DIGIT: new RegExp(`(?=.*\\d){${PASSWORD_DETAILS.DIGIT_LENGTH},}`),
+  HAS_SPECIAL_CHAR: new RegExp(`(?=.*[${PASSWORD_DETAILS.SPC_CHAR}]){${PASSWORD_DETAILS.SPC_LENGTH},}`),
+  IS_VALID: new RegExp(`[A-Za-z\\d${PASSWORD_DETAILS.SPC_CHAR}]{${PASSWORD_DETAILS.MIN_LENGTH},${PASSWORD_DETAILS.MAX_LENGTH}}$`)
 }
 
 const RE_RASSWORD = new RegExp(
-  `${PASSWORD_RE.HAS_LETTER.source}${PASSWORD_RE.HAS_DIGIT.source}${PASSWORD_RE.HAS_SPECIAL_CHAR.source}${PASSWORD_RE.IS_VALID.source}`
+  `${PASSWORD_RE_PART.HAS_LETTER.source}${PASSWORD_RE_PART.HAS_DIGIT.source}${PASSWORD_RE_PART.HAS_SPECIAL_CHAR.source}${PASSWORD_RE_PART.IS_VALID.source}`
 )
 
 
-export const isEmailValid = (email: string): boolean => RE_EMAIL.test(email)
+const isPasswordValid = (password: string): boolean => RE_RASSWORD.test(password)
 
-export const isPasswordValid = (password: string): boolean => RE_RASSWORD.test(password)
+export { isEmailValid, isPasswordValid, }
